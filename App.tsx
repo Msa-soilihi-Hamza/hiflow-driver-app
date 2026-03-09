d569dbd32aba90eec3eb1926b3470933e212bd00
Msa-soilihi-Hamza <hamza.msa-soilihi@laplateforme.io>
2026-03-09 16:45:40 +0100

    chore: configuration de Sentry avec le DSN et les infos du projet

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://28e74a23a06f000a58dbd5f0387267b8@o4511015299055616.ingest.de.sentry.io/4511015300956240',
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
+function App() {
   return <AppNavigator />;
 }
+
+export default Sentry.wrap(App);
