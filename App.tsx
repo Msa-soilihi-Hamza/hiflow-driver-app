d58083d1dafeb203a2751b4f9b221b096b5d1a0d
Msa-soilihi-Hamza <hamza.msa-soilihi@laplateforme.io>
2026-03-12 17:27:08 +0100

    changement sentry

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import * as Sentry from '@sentry/react-native';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 Sentry.init({
-  dsn: 'https://28e74a23a06f000a58dbd5f0387267b8@o4511015299055616.ingest.de.sentry.io/4511015300956240',
-  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  debug: false,
 });
 
 function App() {
